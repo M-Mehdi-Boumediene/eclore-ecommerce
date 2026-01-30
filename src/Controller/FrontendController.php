ab4eec52867460b91154572d6d0f8b37f26a2f28
Mehdi Boumediene <elm3hdi@gmail.com>
2026-01-30 20:16:56 +0100

    Frontend: Products Index

--- src/Controller/FrontendController.php
+++ src/Controller/FrontendController.php
@@ -6,13 +6,26 @@ use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 
 class FrontendController extends AbstractController
 {
     #[Route('/', name: 'app_frontend')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
-        return $this->render('frontend/index.html.twig');
+         $products = $productRepository->findAll();
+        return $this->render('frontend/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+    #[Route('/product/{slug}', name: 'product_show')]
+    public function show(Product $product): Response
+    {
+        // Le paramConverter de Symfony récupère automatiquement le produit via le slug
+        return $this->render('frontend/product/show.html.twig', [
+            'product' => $product,
+        ]);
     }
 
 
