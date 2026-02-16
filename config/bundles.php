250d6110b86c2e51bf5f2d54795ac7b61fa744a5
Mehdi Boumediene <elm3hdi@gmail.com>
2026-02-16 14:26:37 +0100

    Backend: Make User

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
